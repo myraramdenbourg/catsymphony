47fb111285a31728bc8f65ff7cdc5f2a4089a2a2
emilyhwang <eyh2@rice.edu>
2018-09-15 23:19:27 -0500

    drag fixed

--- javascript/index.js
+++ javascript/index.js
@@ -73,16 +73,3 @@ function playSound10() {
     var sound = document.getElementById("audio10");
     sound.play();
 }
-
-$(function() {
-    $( "#draggable1" ).draggable();
-    $( "#draggable2" ).draggable();
-    $( "#draggable3" ).draggable();
-    $( "#draggable4" ).draggable();
-    $( "#draggable5" ).draggable();
-    $( "#draggable6" ).draggable();
-    $( "#draggable7" ).draggable();
-    $( "#draggable8" ).draggable();
-    $( "#draggable9" ).draggable();
-    $( "#draggable10" ).draggable();
-});
