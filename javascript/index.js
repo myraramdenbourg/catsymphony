ef4aa9228199e715faef5e76abef4c89083240c3
emilyhwang <eyh2@rice.edu>
2018-09-15 23:14:37 -0500

    half of As removed

--- javascript/index.js
+++ javascript/index.js
@@ -23,3 +23,66 @@ document.getElementById('date').innerHTML = new Date().toDateString();
 //     window.removeEventListener('mousemove', startResizing, false);
 //     window.removeEventListener('mouseup', stopResizing, false);
 // }
+
+function playSound1(){
+    const sound = document.getElementById("audio1");
+    sound.play();
+}
+
+function playSound2() {
+    const sound = document.getElementById("audio2");
+    sound.play();
+}
+
+function playSound3() {
+    var sound = document.getElementById("audio3");
+    sound.play();
+}
+
+function playSound4() {
+    var sound = document.getElementById("audio3");
+    sound.play();
+}
+
+function playSound5() {
+    var sound = document.getElementById("audio5");
+    sound.play();
+}
+
+function playSound6() {
+    var sound = document.getElementById("audio6");
+    sound.play();
+}
+
+function playSound7() {
+    var sound = document.getElementById("audio7");
+    sound.play();
+}
+
+function playSound8() {
+    var sound = document.getElementById("audio8");
+    sound.play();
+}
+
+function playSound9() {
+    var sound = document.getElementById("audio9");
+    sound.play();
+}
+
+function playSound10() {
+    var sound = document.getElementById("audio10");
+    sound.play();
+}
+
+$(function() {
+    $( "#draggable1" ).draggable();
+    $( "#draggable2" ).draggable();
+    $( "#draggable3" ).draggable();
+    $( "#draggable4" ).draggable();
+    $( "#draggable5" ).draggable();
+    $( "#draggable6" ).draggable();
+    $( "#draggable7" ).draggable();
+    $( "#draggable8" ).draggable();
+    $( "#draggable9" ).draggable();
+    $( "#draggable10" ).draggable();
+});
