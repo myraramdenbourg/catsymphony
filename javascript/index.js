f12777511f27f8deafbd99411b3f9c7e4962a116
emilyhwang <eyh2@rice.edu>
2018-09-16 00:42:17 -0500

    FINISHED!!!

--- javascript/index.js
+++ javascript/index.js
@@ -24,52 +24,143 @@ document.getElementById('date').innerHTML = new Date().toDateString();
 //     window.removeEventListener('mouseup', stopResizing, false);
 // }
 
-function playSound1(){
-    const sound = document.getElementById("audio1");
-    sound.play();
+function playSound1() {
+    var sound = document.getElementById("audio1");
+    var hi = document.getElementById("draggable1");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound2() {
-    const sound = document.getElementById("audio2");
-    sound.play();
+    var sound = document.getElementById("audio2");
+    var hi = document.getElementById("draggable2");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound3() {
     var sound = document.getElementById("audio3");
-    sound.play();
+    var hi = document.getElementById("draggable3");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound4() {
-    var sound = document.getElementById("audio3");
-    sound.play();
+    var sound = document.getElementById("audio4");
+    var hi = document.getElementById("draggable4");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound5() {
     var sound = document.getElementById("audio5");
-    sound.play();
+    var hi = document.getElementById("draggable5");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound6() {
     var sound = document.getElementById("audio6");
-    sound.play();
+    var hi = document.getElementById("draggable6");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound7() {
     var sound = document.getElementById("audio7");
-    sound.play();
+    var hi = document.getElementById("draggable7");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound8() {
     var sound = document.getElementById("audio8");
-    sound.play();
+    var hi = document.getElementById("draggable8");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound9() {
     var sound = document.getElementById("audio9");
-    sound.play();
+    var hi = document.getElementById("draggable9");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
 
 function playSound10() {
     var sound = document.getElementById("audio10");
-    sound.play();
+    var hi = document.getElementById("draggable10");
+    var rect = hi.getBoundingClientRect();
+    // console.log("top" + rect.top + "right" + rect.right + "bottom" + rect.bottom + "left" + rect.left);
+    if (rect.top > 200) {
+        sound.play();
+    }
+    else {
+        sound.pause();
+    }
+
 }
+
