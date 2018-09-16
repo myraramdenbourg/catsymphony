cad0d4225ecfced07987bea0eae80c87fca89e23
emilyhwang <eyh2@rice.edu>
2018-09-15 21:26:53 -0500

    figured out how to play sounds

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,25 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
 
+var audio = new Audio();
+audio.src = './audio/TomCat-Mar_Smith-2055290520.mp3';
+audio.load();
+audio.play();
+
+
+var resizeHandle = document.getElementById('handle');
+var draggable1 = document.getElementById('draggable1');
+resizeHandle.addEventListener('mousedown', initialiseResize, false);
+
+function initialiseResize(e) {
+    window.addEventListener('mousemove', startResizing, false);
+    window.addEventListener('mouseup', stopResizing, false);
+}
+
+function startResizing(e) {
+    draggable1.style.width = (e.clientX - draggable1.offsetLeft) + 'px';
+    draggable1.style.height = (e.clientY - draggable1.offsetTop) + 'px';
+}
+function stopResizing(e) {
+    window.removeEventListener('mousemove', startResizing, false);
+    window.removeEventListener('mouseup', stopResizing, false);
+}
